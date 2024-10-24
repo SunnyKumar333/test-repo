1066534993e0041dc1280f6ebc02760d70ef4155
Sunny Kumar <139466971+SunnyKumar333@users.noreply.github.com>
2024-10-24 06:55:11 +0000

    add greet feture

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const PORT=3030
 app.get("/",(req,res)=>{
   res.send("hello world");
 })
+
+app.get("/greet",(req,res)=>{
+  res.send("Good Morning")
+})
+
 app.listen(PORT,()=>{
   console.log(`server is running on port ${PORT}`)
 })
